81706bf29b8f07b7a6a383b0670002de2bd5f432
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2023-02-06 18:20:53 +0100

    Delete charAt.js

--- javaScript.introduccionClase/src/main/webapp/script/charAt.js
+++
@@ -1,8 +0,0 @@
-function separar(){
-    let palabra = prompt("Introduce una palabra: ");
-    let palabraFinal="";
-    for(var i=0;i< palabra.length;i++){
-        palabraFinal += palabra[i] + "|";
-    }
-    alert(palabraFinal);
-}
